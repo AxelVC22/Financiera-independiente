17e467ce4ab39f3ef7815c80a1d487918bb92765
AxelVC22 <162675163+AxelVC22@users.noreply.github.com>
2025-05-23 23:07:17 -0600

    Update User.cs

--- Model/User.cs
+++ Model/User.cs
@@ -10,7 +10,6 @@ namespace Independiente.Model
     {
         public int EmployeeId { get; set; }
         public int Id { get; set; }
-        public int EmployeeId { get; set; }
         public string Name { get; set; }
         public string Role { get; set; }
         public string Password { get; set; }
