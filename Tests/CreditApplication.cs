1bf34931852747a78188e800b1758cf9693ed873
francoroguez <118698321+roguez26@users.noreply.github.com>
2025-05-23 20:27:01 -0600

    Inicio de pruebas

---
+++ Tests/CreditApplication.cs
@@ -0,0 +1,80 @@
+﻿using Independiente.DataAccess.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class CreditApplication
+    {
+        public ICreditApplicationRepository CreditApplicationRepository { get; set; } = new CreditApplicationRepository();
+
+        [TestMethod]
+        public void TestCountCreditApplicationsSuccess()
+        {
+            int result = 0;
+
+            result = CreditApplicationRepository.CountCreditApplications(new CreditApplicationQuery
+            {
+                PageNumber = 1,
+                PageSize = 10,
+                RFC = "ARPS9288383",
+                Status = "Accepted",
+                FromDate = new DateTime(2025, 05, 07),
+                ToDate = new DateTime(2025, 05, 07),
+            });
+            
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestCountCreditApplicationsSuccessWithoutRFC()
+        {
+            int result = 0;
+
+            result = CreditApplicationRepository.CountCreditApplications(new CreditApplicationQuery
+            {
+                PageNumber = 1,
+                PageSize = 10,
+                Status = "Accepted",
+                FromDate = new DateTime(2025, 05, 07),
+                ToDate = new DateTime(2025, 05, 07),
+            });
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestCountCreditApplicationsSuccessWithoutStatus()
+        {
+            int result = 0;
+
+            result = CreditApplicationRepository.CountCreditApplications(new CreditApplicationQuery
+            {
+                PageNumber = 1,
+                PageSize = 10,
+                RFC = "ARPS9288383",
+                FromDate = new DateTime(2025, 05, 07),
+                ToDate = new DateTime(2025, 05, 07),
+            });
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestCountCreditApplicationsSuccessWithoutDateRange()
+        {
+            int result = 0;
+
+            result = CreditApplicationRepository.CountCreditApplications(new CreditApplicationQuery
+            {
+                PageNumber = 1,
+                PageSize = 10,
+                RFC = "ARPS9288383",
+                Status = "Accepted",
+            });
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5f24c93-2bb7-47b5-a69a-f170e148ef80")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
